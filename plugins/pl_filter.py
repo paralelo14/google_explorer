eec660b0ea8cd2c46940a4895ca5650671f577f7
anarcoder <anarcoder@protonmail.com>
2017-09-30 10:50:42 -0300

    change parameter plugin name

---
+++ plugins/pl_filter.py
@@ -0,0 +1,38 @@
+"""
+Class used to run the exploit filters in the result file
+"""
+import os
+import sys
+
+PATH = os.path.join(os.sep, os.getcwd() + '/' + 'plugins')
+
+
+class Plugins():
+
+    def __init__(self, filters):
+        self.filters = filters
+        self.exploits = self.validate_plugins()
+        self.run_filter()
+
+    def validate_plugins(self):
+        pl_list = [f.rsplit(".")[0] for f in os.listdir(PATH)]
+        filters = self.filters.split(',')
+        exploits = list(set(filters).intersection(set(pl_list)))
+        if exploits:
+            return exploits
+        else:
+            print("\n\033[31m### Error ###")
+            print("\033[33mNone of the plugins you choose "
+                  "were found!!\033[0m\n")
+            print("The current options are:\n")
+            for pl in pl_list:
+                print(pl)
+            sys.exit(1)
+
+    def run_filter(self):
+        try:
+            for pl in self.exploits:
+                pl_path = os.path.join(PATH + '/' + pl + '.py')
+                os.system('python3 ' + pl_path)
+        except:
+            pass
